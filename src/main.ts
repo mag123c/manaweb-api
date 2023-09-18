d121bea05ddb0516005a5efb11e6216cf0687632
mag123c <diehreo@gmail.com>
2023-09-18 12:01:58 +0900

    set nestjs project

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app/app.module';
+import { setupSwagger } from './util/swagger.setup';
+
+async function bootstrap() {
+  // const allowedOrigins = [
+  //   'http://localhost:3000',   
+  //   'http://localhost:6800',
+  // ];
+  process.env.TZ = 'Asia/Seoul';
+  const app = await NestFactory.create(AppModule);
+  setupSwagger(app);
+  await app.listen(3000);
+}
+bootstrap();
