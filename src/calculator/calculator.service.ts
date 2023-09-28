3163a1850d8fed017fe175cb630951ed1af5a260
장재호 <diehreo@gmail.com>
2023-09-28 22:45:08 +0900

    prod server ws del

--- src/calculator/calculator.service.ts
+++ src/calculator/calculator.service.ts
@@ -4,25 +4,29 @@ import { Injectable } from '@nestjs/common';
 export class CalculatorService {
 
   async calculate(price: number, percent: number, time: number, type: string) {
-    if(time <= 0 || price <= 0) return null;
+    if (time <= 0 || price <= 0) return null;
 
     let priceArr = [price];
     let data = new Array();
-    for(let i = 1; i < time; i ++) {
-        const beforePrice = priceArr[i - 1];
-        const currentPrice = Math.round(beforePrice + (beforePrice / 100 * percent));
-        const priceThanBefore = currentPrice - beforePrice;
-        const totalPercent = ((currentPrice - price) / (price / 100)).toFixed(2);
+    let totalRevenue = 0;
+    for (let i = 1; i <= time; i++) {
+      const beforePrice = +priceArr[i - 1];
+      const interest = beforePrice * (+percent / 100);
+      const currentPrice = Math.round(beforePrice + interest);
+      const revenue = currentPrice - beforePrice;
+      const totalPercent = ((currentPrice - price) / (price / 100)).toFixed(2);
+      totalRevenue += revenue;
 
-        priceArr.push(currentPrice);
-        data.push({ 
-            'tmp': i + type,
-            'priceThanBefore': priceThanBefore,
-            'currentPrice': currentPrice,
-            'totalPercent': totalPercent + '%',
-        });
+      priceArr.push(currentPrice);
+      data.push({
+        'tmp': i,
+        'revenue': revenue,
+        'currentPrice': currentPrice,
+        'totalPercent': totalPercent,
+      });
     }
-    
-    return { priceArr, data };
+
+    return { data, totalRevenue };
   }
 }
+
