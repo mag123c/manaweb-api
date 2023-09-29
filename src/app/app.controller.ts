065049aa0bce573243304bba682cf9f20d4198af
장재호 <diehreo@gmail.com>
2023-09-29 12:17:07 +0900

    test

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,7 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('/api')
+@Controller('/')
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get()
+    checkHealth() {
+      return { status: 'ok' };
+    }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,13 @@ import { AppModule } from './app/app.module';
 import { setupSwagger } from './util/swagger.setup';
 
 async function bootstrap() {
-  // const allowedOrigins = [
-  //   'http://localhost:3000',   
-  //   'http://localhost:6800',
-  // ];
+  const allowedOrigins = [
+     'http://localhost:3000',   
+  ];
   process.env.TZ = 'Asia/Seoul';
   const app = await NestFactory.create(AppModule, { cors: true });
   setupSwagger(app);
   await app.listen(8080);
 }
+
 bootstrap();
