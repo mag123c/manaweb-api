c92638e7b7ed803a24550bb89fb5ee87afa35e35
장재호 <diehreo@gmail.com>
2023-09-30 21:08:08 +0900

    test

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,12 @@ import { AppModule } from './app/app.module';
 import { setupSwagger } from './util/swagger.setup';
 
 async function bootstrap() {
-  const allowedOrigins = [
-     'http://localhost:3000',   
-  ];
   process.env.TZ = 'Asia/Seoul';
   const app = await NestFactory.create(AppModule, { cors: true });
   setupSwagger(app);
-  await app.listen(3065);
+  await app.listen(3065, () => {
+    console.log('server running');
+  });
 }
 
 bootstrap();
