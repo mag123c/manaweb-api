59d0d8b932dc172763a9bff3f02c9cfac740c227
mag123c <diehreo@gmail.com>
2023-10-21 11:16:45 +0900

    전역일 계산기 에러해결

--- src/calculator/calculator.service.ts
+++ src/calculator/calculator.service.ts
@@ -4,6 +4,14 @@ import { CPResponse } from './response/cp.response';
 @Injectable()
 export class CalculatorService {
 
+  /**
+   * 복리계산기
+   * @param price 
+   * @param percent 
+   * @param time 
+   * @param type 
+   * @returns 
+   */
   async calculateCp(price: number, percent: number, time: number, type: string) {
     if (Number(price) <= 0 || Number(percent) <= 0 || Number(time) <= 0) {
       throw new BadRequestException('BadrequestException:: NPE');
@@ -33,6 +41,12 @@ export class CalculatorService {
     return { data, totalRevenue };
   }
 
+  /**
+   * 전역일 계산기
+   * @param enlistDay: 입대일
+   * @param months: 복무기간
+   * @returns 
+   */
   async military(enlistDay: string, months: number) {
     if (!enlistDay || !months) throw new BadRequestException('BadrequestException:: NPE');
 
@@ -44,12 +58,12 @@ export class CalculatorService {
 
   async calculateDischargeDate(enlistDay: string, months: number) {
     const enlistDayToDate = new Date(enlistDay);
-    enlistDayToDate.setMonth(enlistDayToDate.getMonth() + months);
+    enlistDayToDate.setMonth(enlistDayToDate.getMonth() + +months);
     enlistDayToDate.setDate(enlistDayToDate.getDate() - 1);
-    
     let year = enlistDayToDate.getFullYear();
-    let month = enlistDayToDate.getMonth() + 1; // 월은 0부터 시작하므로 +1을 해줍니다.
+    let month = enlistDayToDate.getMonth() + 1;
     let day = enlistDayToDate.getDate();
+    console.log(year, month, day);
     
     // month와 day가 한 자릿수인 경우, 앞에 0을 추가합니다.
     const monthStr = month < 10 ? '0' + month : String(month);
