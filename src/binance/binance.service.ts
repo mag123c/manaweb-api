02ef51a27f935b471d060c54640eb688e15c3d0b
mag123c <diehreo@gmail.com>
2023-11-14 18:30:15 +0900

    test

--- src/binance/binance.service.ts
+++ src/binance/binance.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class BinanceService {}
+export class BinanceService {
+    future_url: string;
+
+    constructor(){
+        this.future_url = 'https://fapi.binance.com/fapi/v1/';
+    }
+
+    async getSymbol() {
+        const symbolURL = 'exchangeInfo';
+    }
+}
