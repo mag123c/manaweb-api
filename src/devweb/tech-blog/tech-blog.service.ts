f536831c4d6eeb57e782cc65128235a29556f97c
mag123c <diehreo@gmail.com>
2023-11-28 10:42:16 +0900

    tech blog

--- src/devweb/tech-blog/tech-blog.service.ts
+++ src/devweb/tech-blog/tech-blog.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { promisify } from 'util';
+
 
 @Injectable()
 export class TechBlogService {
-    og: any;
+    ogs: any;
+    ogsAsync: any;
     constructor() {
-        this.og = require('open-graph');
+        this.ogs = require('open-graph-scraper');
+        this.ogsAsync = promisify(this.ogs);
     }
     //OG 태그 가져와서 커스텀 DIV 만들어주기
     // 1. crawling > posting url get
@@ -18,16 +22,28 @@ export class TechBlogService {
     //   });
     // 3. return data.
     async getDataByName(link: string) {
-        const blogCrawled = await this.crawlByName(link);
-        const ogData = await this.getOgData(blogCrawled);
+        const selectedLinkOgData = await this.getOgData(link);
+        // const blogCrawled = await this.crawlByName(link);
+        // const crawledOgData = await this.getOgData(blogCrawled);
 
-        return ogData;
+        console.log('test', selectedLinkOgData);
+        return selectedLinkOgData;
     }
 
     async crawlByName(name: string) {
-        
+
     }
-    async getOgData(blogCrawled: any) {
-        
-    }    
+
+    async getOgData(url: any) {
+        const options = { url: url, timeout: 4000 };
+
+        try {
+            const data = await this.ogs(options);
+            const { error, html, result, response } = data;
+            console.log(result);
+            return result;
+        } catch (error) {
+            console.error(error);
+        }
+    }
 }
